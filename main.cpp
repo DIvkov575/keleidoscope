f4efe63da274f23b4970d24512ad050f8ddec3f7
Divkov575 <dmitriy@ivkov.net>
2024-01-25 20:02:39 -0500

    implemented lexer

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ static int gettok() {
     while (isspace(LastChar))
         LastChar = getchar();
 
+    // get and build keyword -> match
     if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
         IdentifierStr = LastChar;
         while (isalnum((LastChar = getchar())))
@@ -36,6 +37,37 @@ static int gettok() {
             return tok_extern;
         return tok_identifier;
     }
+
+    // get 'ident' 🤓
+    if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    // comments
+    if (LastChar == '#') {
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok();
+    }
+
+    // Check for end of file.  Don't eat the EOF. 🧌
+    if (LastChar == EOF)
+        return tok_eof;
+
+    // Otherwise, just return the character as its ascii value.
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
 }
 
 int main() {
