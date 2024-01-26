3200bcf52634a1c69464c3988ecc7f9b20974aa3
Divkov575 <dmitriy@ivkov.net>
2024-01-25 21:55:46 -0500

    added main repl loop + added codegen

--- main.cpp
+++ main.cpp
@@ -147,5 +147,19 @@ Function *FunctionAST::codegen() {
 
 
 int main() {
+    // Install standard binary operators.
+    // 1 is lowest precedence.
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 20;
+    BinopPrecedence['*'] = 40; // highest.
+
+    // Prime the first token.
+    fprintf(stderr, "ready> ");
+    getNextToken();
+
+    // Run the main "interpreter loop" now.
+    MainLoop();
+
     return 0;
 }
