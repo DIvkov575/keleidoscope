8d0bcc201e7b0d9d06b78a618ac4f03b668be805
Divkov575 <dmitriy@ivkov.net>
2024-01-28 19:58:48 -0500

    refactor + completion

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
+// lexer headers
+#include "lexer/lexer.h"
+#include "lexer/token.h"
+
+// AST headers
+#include "ast/BinaryExprAST.h"
+#include "ast/CallExprAST.h"
+#include "ast/ExprAST.h"
+#include "ast/FunctionAST.h"
+#include "ast/NumberExprAST.h"
+#include "ast/PrototypeAST.h"
+#include "ast/VariableExprAST.h"
+
+// parser headers
+#include "parser/parser.h"
+
+// logger headers
+#include "logger/logger.h"
+
+// kaleidoscope headers
+#include "kaleidoscope/kaleidoscope.h"
+
+// LLVM headers
 #include "llvm/ADT/APFloat.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
@@ -9,6 +32,8 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
+
+// stdlib headers
 #include <algorithm>
 #include <cctype>
 #include <cstdio>
@@ -17,157 +42,82 @@
 #include <memory>
 #include <string>
 #include <vector>
-#include <iostream>
-#include "ast.cpp"
+
 using namespace llvm;
 
-static std::unique_ptr<LLVMContext> TheContext;
-static std::unique_ptr<IRBuilder<>> Builder(TheContext);
-static std::unique_ptr<Module> TheModule;
-static std::map<std::string, Value *> NamedValues;
-static std::unique_ptr<KaleidoscopeJIT> TheJIT;
-
-Value *LogErrorV(const char *Str) {
-    LogError(Str);
-    return nullptr;
-}
-
-Value *NumberExprAST::codegen() {
-    return ConstantFP::get(*TheContext, APFloat(Val));
-}
-
-Value *VariableExprAST::codegen() {
-    // Look this variable up in the function.
-    Value *V = NamedValues[Name];
-    if (!V)
-        LogErrorV("Unknown variable name");
-    return V;
-}
-
-Value *BinaryExprAST::codegen() {
-    Value *L = LHS->codegen();
-    Value *R = RHS->codegen();
-    if (!L || !R)
-        return nullptr;
-
-    switch (Op) {
-        case '+':
-            return Builder->CreateFAdd(L, R, "addtmp");
-        case '-':
-            return Builder->CreateFSub(L, R, "subtmp");
-        case '*':
-            return Builder->CreateFMul(L, R, "multmp");
-        case '<':
-            L = Builder->CreateFCmpULT(L, R, "cmptmp");
-        // Convert bool 0/1 to double 0.0 or 1.0
-        return Builder->CreateUIToFP(L, Type::getDoubleTy(TheContext),
-                                     "booltmp");
-        default:
-            return LogErrorV("invalid binary operator");
+static void HandleDefinition() {
+  if (auto FnAST = ParseDefinition()) {
+    if (auto *FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read function definition:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
     }
+  } else {
+    getNextToken();
+  }
 }
 
-Value *CallExprAST::codegen() {
-    // Look up the name in the global module table.
-    Function *CalleeF = TheModule->getFunction(Callee);
-    if (!CalleeF)
-        return LogErrorV("Unknown function referenced");
-
-    // If argument mismatch error.
-    if (CalleeF->arg_size() != Args.size())
-        return LogErrorV("Incorrect # arguments passed");
-
-    std::vector<Value *> ArgsV;
-    for (unsigned i = 0, e = Args.size(); i != e; ++i) {
-        ArgsV.push_back(Args[i]->codegen());
-        if (!ArgsV.back())
-            return nullptr;
+static void HandleExtern() {
+  if (auto ProtoAST = ParseExtern()) {
+    if (auto *FnIR = ProtoAST->codegen()) {
+      fprintf(stderr, "Read extern:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
     }
-
-    return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+  } else {
+    getNextToken();
+  }
 }
 
-Function *PrototypeAST::codegen() {
-    // Make the function type:  double(double,double) etc.
-    std::vector<Type*> Doubles(Args.size(),
-                               Type::getDoubleTy(*TheContext));
-    FunctionType *FT =
-      FunctionType::get(Type::getDoubleTy(*TheContext), Doubles, false);
-
-    Function *F =
-      Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
-
-    // Set names for all arguments.
-    unsigned Idx = 0;
-    for (auto &Arg : F->args())
-        Arg.setName(Args[Idx++]);
-
-    return F;
-}
-
-Function *FunctionAST::codegen() {
-    // First, check for an existing function from a previous 'extern' declaration.
-    Function *TheFunction = TheModule->getFunction(Proto->getName());
-
-    if (!TheFunction)
-        TheFunction = Proto->codegen();
-
-    if (!TheFunction)
-        return nullptr;
-
-    if (!TheFunction->empty())
-        return (Function*)LogErrorV("Function cannot be redefined.");
-
-    // Create a new basic block to start insertion into.
-    BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
-    Builder->SetInsertPoint(BB);
-
-    // Record the function arguments in the NamedValues map.
-    NamedValues.clear();
-    for (auto &Arg : TheFunction->args())
-        NamedValues[std::string(Arg.getName())] = &Arg;
-
-    if (Value *RetVal = Body->codegen()) {
-        // Finish off the function.
-        Builder.CreateRet(RetVal);
-
-        // Validate the generated code, checking for consistency.
-        verifyFunction(*TheFunction);
-
-        // Optimize the function.
-        TheFPM->run(*TheFunction, *TheFAM);
-
-        return TheFunction;
+static void HandleTopLevelExpression() {
+  if (auto FnAST = ParseTopLevelExpr()) {
+    if (auto *FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read top-level expression:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
     }
-
-    // Error reading body, remove function.
-    TheFunction->eraseFromParent();
-    return nullptr;
-
-
-    // TODO!: has bug - extern functions will take precedence over local definitions -> arg mismatches not detected => fup
+  } else {
+    getNextToken();
+  }
 }
 
+static void MainLoop() {
+  while (true) {
+    fprintf(stderr, "ready> ");
 
+    switch (CurTok) {
+      case tok_eof:
+      return;
+      case ';':
+      getNextToken();
+      break;
+      case tok_def:
+      HandleDefinition();
+      break;
+      case tok_extern:
+      HandleExtern();
+      break;
+      default:
+      HandleTopLevelExpression();
+      break;
+    }
+  }
+}
 
 int main() {
-    InitializeNativeTarget();
-    InitializeNativeTargetAsmPrinter();
-    InitializeNativeTargetAsmParser();    // Install standard binary operators.
+  BinopPrecedence['<'] = 10;
+  BinopPrecedence['+'] = 20;
+  BinopPrecedence['-'] = 20;
+  BinopPrecedence['*'] = 40;
 
-    // 1 is lowest precedence.
-    BinopPrecedence['<'] = 10;
-    BinopPrecedence['+'] = 20;
-    BinopPrecedence['-'] = 20;
-    BinopPrecedence['*'] = 40; // highest.
+  fprintf(stderr, "ready> ");
+  getNextToken();
 
-    // Prime the first token.
-    fprintf(stderr, "ready> ");
-    getNextToken();
+  TheModule = std::make_unique<Module>("My awesome JIT", TheContext);
 
-    TheJIT = std::make_unique<KaleidoscopeJIT>();
+  MainLoop();
 
-    // Run the main "interpreter loop" now.
+  TheModule->print(errs(), nullptr);
 
-    return 0;
+  return 0;
 }
