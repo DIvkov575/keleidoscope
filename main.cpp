2d0c174cddf64b130442bdbf979058d25a034be9
Divkov575 <dmitriy@ivkov.net>
2024-01-29 08:27:18 -0500

    created conditions for JIT vs AOT based cli args

--- main.cpp
+++ main.cpp
@@ -31,100 +31,108 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <__filesystem/path.h>
 
 using namespace llvm;
 
 static void HandleDefinition() {
-  if (auto FnAST = ParseDefinition()) {
-    if (auto *FnIR = FnAST->codegen()) {
-      fprintf(stderr, "Read function definition:");
-      FnIR->print(errs());
-      fprintf(stderr, "\n");
+    if (auto FnAST = ParseDefinition()) {
+        if (auto *FnIR = FnAST->codegen()) {
+            fprintf(stderr, "Read function definition:");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
+    } else {
+        getNextToken();
     }
-  } else {
-    getNextToken();
-  }
 }
 
 static void HandleExtern() {
-  if (auto ProtoAST = ParseExtern()) {
-    if (auto *FnIR = ProtoAST->codegen()) {
-      fprintf(stderr, "Read extern:");
-      FnIR->print(errs());
-      fprintf(stderr, "\n");
+    if (auto ProtoAST = ParseExtern()) {
+        if (auto *FnIR = ProtoAST->codegen()) {
+            fprintf(stderr, "Read extern:");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
+    } else {
+        getNextToken();
     }
-  } else {
-    getNextToken();
-  }
 }
 
 static void HandleTopLevelExpression() {
-  if (auto FnAST = ParseTopLevelExpr()) {
-    if (auto *FnIR = FnAST->codegen()) {
-      fprintf(stderr, "Read top-level expression:");
-      FnIR->print(errs());
-      fprintf(stderr, "\n");
+    if (auto FnAST = ParseTopLevelExpr()) {
+        if (auto *FnIR = FnAST->codegen()) {
+            fprintf(stderr, "Read top-level expression:");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
+    } else {
+        getNextToken();
     }
-  } else {
-    getNextToken();
-  }
 }
 
 static void MainLoop() {
-  while (true) {
-    fprintf(stderr, "> ");
+    while (true) {
+        fprintf(stderr, "> ");
 
-    switch (CurTok) {
-      case tok_eof:
-        return;
-      case ';':
-        getNextToken();
-        break;
-      case tok_def:
-        HandleDefinition();
-        break;
-      case tok_extern:
-        HandleExtern();
-        break;
-      default:
-        HandleTopLevelExpression();
-        break;
+        switch (CurTok) {
+            case tok_eof:
+                return;
+            case ';':
+                getNextToken();
+                break;
+            case tok_def:
+                HandleDefinition();
+                break;
+            case tok_extern:
+                HandleExtern();
+                break;
+            default:
+                HandleTopLevelExpression();
+                break;
+        }
     }
-  }
 }
 
-int main(int argc, char* argv[]) {
-  BinopPrecedence['<'] = 10;
-  BinopPrecedence['+'] = 20;
-  BinopPrecedence['-'] = 20;
-  BinopPrecedence['*'] = 40;
-
-  if (argc > 1) {
-    // for (auto t: argv) {
-    //   std::cout << t;
-    // }
-  std::string args = argv;
-  std::cout << args;
-
-  }
+int main(int argc, char *argv[]) {
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 20;
+    BinopPrecedence['*'] = 40;
 
 
+    if (argc > 1) {
+        std::string input(argv[1]);
+        if (input == "repl") {
+            fprintf(stderr, "ready> ");
+            getNextToken();
+
+            TheModule = std::make_unique<Module>("My awesome JIT", TheContext);
 
 
+            fprintf(stderr, "CT> ");
+            MainLoop();
+
+            TheModule->print(errs(), nullptr);
+        } else {
+            const std::string extension = ((std::__fs::filesystem::path) input).extension();
+            if (extension == ".tl") {
+                FILE *file = fopen(input.c_str(), "r");
+                if (file == NULL) {
+                    std::cout << "Please enter a valid keleidoscope file or 'repl'" << std::endl;
+                    return EXIT_FAILURE;
+                } else {
+                    // compile
+
+                    getNextToken();
+                    TheModule = std::make_unique<Module>("My awesome JIT", TheContext);
+                    MainLoop();
+                    TheModule->print(errs(), nullptr);
+                }
+            }
+        }
+    }
 
 
-
-  // fprintf(stderr, "ready> ");
-  // getNextToken();
-  //
-  // TheModule = std::make_unique<Module>("My awesome JIT", TheContext);
-  //
-  //
-  // fprintf(stderr, "CT> ");
-  // MainLoop();
-  //
-  // TheModule->print(errs(), nullptr);
-  //
-  return 0;
+    return 0;
 }
-
